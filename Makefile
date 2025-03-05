faf6f6ae3e3efd02e7f24516e487c58e99a70e26
CarolShingai <carolineshingai@gmail.com>
2025-03-05 00:10:32 +0000

    makefile, libs, includes, pronto

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
 NAME = cub3d
+
+MAKEFLAGS += --no-print-directory
 FLAGS = -Wall -Wextra -Werror -Wunreachable-code -Ofast -g3
+
+GREEN = $(shell tput setaf 2)
+YELLOW = $(shell tput setaf 3)
+RESET = $(shell tput sgr0)
+
 LIBMLX = libs/MLX42
-LIBS = $(LIBMLX)/build/libmlx42.a -ldl -lglfw -pthread -lm
+LIBFT = libs/libft
+FT_PRINTF = libs/ft_printf
+LIBS = $(LIBFT)/libft.a $(FT_PRINTF)/libftprintf.a $(LIBMLX)/build/libmlx42.a -ldl -lglfw -pthread -lm
 
 SRCS = ${addprefix srcs/, \
 			main.c \
@@ -13,27 +22,45 @@ OBJS = $(SRCS:srcs/%.c=obj/%.o)
 # RULES
 all: $(NAME)
 
-$(NAME): libmlx $(OBJS)
-	@cc $(FLAGS) $(OBJS) $(LIBS) -o $(NAME)
+$(NAME): libmlx  libft ft_printf $(OBJS)
 	@echo "Compiling cub3d"
+	@cc $(FLAGS) $(OBJS) $(LIBS) -o $(NAME)
+	@echo "$(GREEN)cub3d is read!✅$(RESET)"
 
 obj/%.o: srcs/%.c
 	@mkdir -p obj
 	@cc $(FLAGS) -c $< -o $@
 
+# Building libraries
+libft:
+	@make -C $(LIBFT) all
+	@echo "Libft is read!"
+
+ft_printf:
+	@make -C $(FT_PRINTF) all
+
 libmlx:
-	@echo "compiling graphics..."
+	@echo "compiling MLX42..."
 	@cmake $(LIBMLX) -B $(LIBMLX)/build > /dev/null
 	@make -C $(LIBMLX)/build -j4 --no-print-directory > /dev/null
+	@echo "Libmlx is read!"
 
+# cleaning object files
 clean: 
 	@echo "Removing objects..."
 	@rm -rf obj
 	@rm -rf $(LIBMLX)/build
+	@make clean -C $(LIBFT) > /dev/null
+	@make clean -C $(FT_PRINTF) > /dev/null
+	@echo "$(YELLOW)Object cleaned!$(RESET)"
 
+# cleaning executable file
 fclean: clean
-	@echo "Removing Executables"
+	@echo "Removing Executables..."
 	@rm -rf $(NAME)
+	@make fclean -C $(LIBFT)
+	@make fclean -C $(FT_PRINTF)
+	@echo "$(YELLOW)Cleaning complete!$(RESET)"
 
 re: fclean all
 
---
+++ libs/libft/ft_printf.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 16:12:59 by cshingai          #+#    #+#             */
+/*   Updated: 2024/12/20 16:14:08 by cshingai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+#include "ft_printf.h"
+
+int	print_format(const char *str, va_list args, int idx)
+{
+	if (str[idx] == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (str[idx] == 'd' || str[idx] == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	else if (str[idx] == 'u')
+		return (ft_putnbr_dec(va_arg(args, int)));
+	else if (str[idx] == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (str[idx] == 'x')
+		return (ft_putnbr_hex(va_arg(args, unsigned int), 0));
+	else if (str[idx] == 'X')
+		return (ft_putnbr_hex(va_arg(args, unsigned int), 1));
+	else if (str[idx] == 'p')
+		return (ft_put_pointer(va_arg(args, unsigned long), 0));
+	else if (str[idx] == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	int		num_char;
+	va_list	args;
+
+	i = 0;
+	num_char = 0;
+	if (str == 0)
+		return (-1);
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%' && str[i + 1])
+		{
+			num_char += print_format(str, args, i + 1);
+			i++;
+		}
+		else
+			num_char += ft_putchar(str[i]);
+		i++;
+	}
+	va_end(args);
+	return (num_char);
+}
